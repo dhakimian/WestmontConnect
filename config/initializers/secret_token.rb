5ab9c74ecc7550a43a04c2525ccf52b69c6fa9f3
XanderStrike <XanderStrike@gmail.com>
2012-04-04 19:25:21 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wmconnect::Application.config.secret_token = '21e5f50b1b2ea997c1dc227761318b2f52440ec5efa0ab3a42cf91820761a85823c38ea8ab96b9fda401e956486abf671dc3a3b2db40b3c01198411873c4ae46'
